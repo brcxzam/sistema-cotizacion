f5011ed6a4e60381b077ad89f5efaba85e4df569
David Baruc Zamorano Nolasco <deyvith.lol@gmail.com>
2021-12-09 20:21:31 -0600

    feat: Conexión con mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductosModule } from './productos/productos.module';
+import { PlazosModule } from './plazos/plazos.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://cdt-test:probandocdt@cluster0.touto.mongodb.net/sistemaCotizacion?retryWrites=true&w=majority',
+      // 'mongodb://localhost:27017/sistemaCotizacion',
+    ),
+    ProductosModule,
+    PlazosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
